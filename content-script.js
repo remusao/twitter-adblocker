ee3f34932cac02306a3fd80f9808e98772010557
Rémi Berson <remi@cliqz.com>
2020-08-11 15:18:47 +0200

    Simplify logic

--- content-script.js
+++ content-script.js
@@ -1,28 +1,23 @@
-function removeNode(root) {
-  let node = root;
-  for (
-    let level = 0;
-    node !== document.body && node.parentNode !== null;
-    level += 1
-  ) {
-    const testid = node.getAttribute('data-testid');
-    if (
-      testid !== null &&
-      (testid === 'placementTracking' ||
-        testid === 'trend' ||
-        testid === 'UserCell')
-    ) {
-      node.parentNode.removeChild(node);
-      break;
-    }
-    node = node.parentNode;
-  }
-}
-
 function removeAds(root) {
-  for (const node of root.querySelectorAll('span')) {
+  for (let node of root.querySelectorAll('span')) {
     if (node.textContent === 'Promoted') {
-      removeNode(node);
+      for (
+        let level = 0;
+        node !== document.body && node.parentNode !== null;
+        level += 1
+      ) {
+        const testid = node.getAttribute('data-testid');
+        if (
+          testid !== null &&
+          (testid === 'placementTracking' ||
+            testid === 'trend' ||
+            testid === 'UserCell')
+        ) {
+          node.parentNode.removeChild(node);
+          break;
+        }
+        node = node.parentNode;
+      }
     }
   }
 }
@@ -32,14 +27,7 @@ setTimeout(removeAds, 0, document);
 const observer = new MutationObserver((mutationList) => {
   for (const mutation of mutationList) {
     if (mutation.addedNodes.length !== 0) {
-      for (const node of mutation.addedNodes) {
-        if (
-          (node.tagName === 'DIV' || node.tagName === 'ASIDE') &&
-          node.textContent.includes('Promoted')
-        ) {
-          removeAds(node);
-        }
-      }
+      removeAds(mutation.target);
     }
   }
 });
